9dcc5d9cddb0a8b8bc43188d27d9bf815a32897c
alpez <alpez@DESKTOP-49NFB9F>
2020-10-16 19:07:25 +0200

    Movimiento basico de Terminator, no definitivo

--- src/PROPRobot/Terminator.java
+++ src/PROPRobot/Terminator.java
@@ -5,6 +5,7 @@
  */
 package PROPRobot;
 
+import static java.lang.Math.sqrt;
 import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
 import robocode.ScannedRobotEvent;
@@ -17,12 +18,49 @@ import robocode.ScannedRobotEvent;
 public class Terminator extends AdvancedRobot{
     
     public void run(){
-        
+       double x = getBattleFieldWidth();
+       double y = getBattleFieldHeight();
+        System.out.println("=>"+ x +"=>"+ y);
+       while(true){
+        //turnLeft(getHeading());
+        while(((getX()<= x/2) && (getY()<= y/2)) ||((getX()>= (x/2) && (getY()<= y/2)))){
+            System.out.println("Q1");
+            double gir = 360 - getHeading();
+            turnRight(gir);
+            ahead(y - getY()- (Math.random()*50+20 ));
+
+            //execute();
+        }
+
+        while((getX()<= x/2) && (getY()>= y/2)){
+            System.out.println("Q3");
+            double gir = 135 - getHeading();
+            turnRight(gir);
+            ahead(sqrt(((x-getX())*(x-getX())) + ((y-getY())*(y-getY())))- (Math.random()*50+20 ));
+
+
+            //execute();
+        }
+        while((getX()>= x/2 ) && (getY()>= y/2) ){
+            System.out.println("Q4");
+            double gir = 225 - getHeading();
+            turnRight(gir);
+            ahead(sqrt(((x-getX())*(x-getX())) + ((y-getY())*(y-getY())))- (Math.random()*50+20 ) );
+
+            //execute();
+        }
+        /*System.out.println("Q0");
+        double gir = 225 - getHeading();
+        turnRight(gir);
+        ahead(sqrt(y*y + x*x));*/
+       }
+       //execute();
+
     }
     public void onScannedRobot(ScannedRobotEvent e){
-        
+        //fire(1);
     }
     public void inHitByBullet (HitByBulletEvent e){
-        
+       // turnLeft(180);
     }
 }
